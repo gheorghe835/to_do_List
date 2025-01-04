9ee92c6b0481c321da1b8e12c4ec78575cea82a0
gheorghe835 <gheorghe835@gmail.com>
2025-01-04 20:22:12 +0200

    Add: Individual class from the task list., constructor, Method to set your pregnancy status, Method to check if the task is completed

--- src/todoList/Task.java
+++ src/todoList/Task.java
@@ -1,4 +1,25 @@
 package todoList;
 
+// Individual class from the task list.
 public class Task {
+    private String description;
+    private boolean completed;
+
+    //constructor
+
+    public Task(String description, boolean completed) {
+        this.description = description;
+        this.completed = completed;
+    }
+
+    //Method to set your pregnancy status
+    public void setCompleted(boolean completed){
+        this.completed = completed;
+    }
+
+    //Method to check if the task is completed
+
+    public boolean isCompleted() {
+        return completed;
+    }
 }
